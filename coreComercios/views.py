1690ced0635f91a01152e911336a77aa413eeda2
Gilberth Monge <gilmonge1996@mail.com>
2020-10-11 11:53:03 -0600

    Se trabaja en los productos

--- coreComercios/views.py
+++ coreComercios/views.py
@@ -8,19 +8,40 @@ def home(request):
     return render(request, "coreComercios/home.html")
 
 def comercio (request, comercio_slug):
+    #trae el comercio si existe
     comercio = get_object_or_404(Comercio, slug=comercio_slug)
 
     # convertimos el contenido json en un diccionario python
     comercio.redessociales = json.loads(comercio.redessociales)
     comercio.contacto      = json.loads(comercio.contacto)
 
-    producto = Producto.objects.filter(comercio=comercio.id)
+    # trae los productos relacionados al comercio
+    productos = Producto.objects.filter(comercio=comercio.id, estado=True)
 
     datos = {
         'comercio':comercio,
-        'producto':producto,
+        'productos':productos,
     }
     return render(request, "coreComercios/comercio.html", datos)
 
 def producto(request, pk, prod_slug):
-    return render(request, "coreComercios/producto.html")
+    # trae el producto si existe
+    producto = get_object_or_404(Producto, id=pk)
+
+    # trae el comercio respectivo al producto
+    comercio = Comercio.objects.filter(id=producto.comercio.id)[0]
+
+    # convertimos el contenido json en un diccionario python
+    comercio.redessociales = json.loads(comercio.redessociales)
+    comercio.contacto      = json.loads(comercio.contacto)
+
+    # trae los productos relacionados al comercio
+    productos = Producto.objects.filter(comercio=comercio.id, estado=True).exclude(id = pk)
+
+    datos = {
+        'producto':producto,
+        'comercio':comercio,
+        'productos':productos,
+    }
+    
+    return render(request, "coreComercios/producto.html", datos)
